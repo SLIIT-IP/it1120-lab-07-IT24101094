f3012b063325ae3a6ca2e57ae2442b78993f90ca
IT24101094 <it24101094@my.sliit.lk>
2024-10-05 20:08:00 +0530

    Add files via upload

---
+++ Q2/IT24101094Lab7Q2A.java
@@ -0,0 +1,12 @@
+public class IT24101094Lab7Q2A{
+public static void main(String[] args){
+int l,s;
+	for (l=1;l<=5;l++){
+		for(s=1;s<=5;s++){
+	System.out.print("$ ");
+}
+	System.out.print("\n");
+}
+
+}
+}
